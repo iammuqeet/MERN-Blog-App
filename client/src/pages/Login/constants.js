fc588b07929caaed5309d110a2516fa523b21333
iammuqeet <iammuqeetahmad@gmail.com>
2023-12-24 19:16:16 +0500

    Basic UI implementation * Created the UI for Home, Login and Register page for blog app * Created Routes using react router * Uses Material-UI for UI components

---
+++ client/src/pages/Login/constants.js
@@ -0,0 +1,4 @@
+export const strings = {
+  redirectToRegister: "Don't have an account? Sign Up",
+  forgetPassword: "Forgot password?",
+};
---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,102 @@
+import {
+  Avatar,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+  Box,
+  Typography,
+  Container,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import { Link as RouterLink } from "react-router-dom";
+
+import ButtonComponent from "../../components/Button";
+import { strings } from "./constants";
+
+const defaultTheme = createTheme();
+
+export default function Login() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Login
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <ButtonComponent buttonText="Login" buttonVariant="contained" />
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  {strings.forgetPassword}
+                </Link>
+              </Grid>
+              <Grid item>
+                <RouterLink
+                  to="/register"
+                  style={{ color: "#1976d2", fontSize: "14px" }}
+                >
+                  {strings.redirectToRegister}
+                </RouterLink>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  );
+}
